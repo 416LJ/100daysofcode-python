81ebf2625e0c17a2b0e7c55473c58222e509ae67
Laxsan Jeya <l.jeya86@gmail.com>
2021-11-21 00:26:57 -0500

    added dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,28 @@
+bag = {
+    "one" : "hello",
+    "two" : "world"
+}
+
+counts = dict()
+names = ["ok", "thanks", "yes", "ok"]
+for name in names:
+    counts[name] = counts.get(name,0) + 1
+print(counts)
+
+name = input("enter a file name : ")
+fileOpen = open(name)
+count = dict()
+for line in fileOpen:
+    words = line.split()
+    for word in words:
+        count[word] = count.get(word, 0) + 1
+bigCount = 0
+bigWord = 0
+for word, counts in count.items():
+    print(count[word])
+    if bigCount is None or counts > bigCount:
+        bigWord = word
+        bigCount = counts
+print(bigCount, bigWord)
+
+
