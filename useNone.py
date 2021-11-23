cdf0cd01d8b30d441b609f16f1e8cc58e27c16ae
Laxsan Jeya <l.jeya86@gmail.com>
2021-11-23 12:50:23 -0500

    chapter regex.py

--- useNone.py
+++ useNone.py
@@ -1,6 +1,9 @@
+"""
+ Below is code to find the smallest value from a list of values.
+"""
 smallest = None
 print("Before:", smallest)
-for itervar in [41, 12, 9, 74, 1, 15]:
+for itervar in [3, 41, 12, 9, 74, 15]:
     if smallest is None or itervar < smallest:
         smallest = itervar
     print("Loop:", itervar, smallest)
